5452f2f53ab6ccbe997b03a03dca9b2182d77498
switchblade <terekhin.mikhail.01@gmail.com>
2023-05-25 19:25:01 -0700

    Updated EPOLL test

--- test/test_epoll.cpp
+++ test/test_epoll.cpp
@@ -4,6 +4,7 @@
 
 #include <string_view>
 #include <rod/io.hpp>
+#include <fcntl.h>
 
 #include "common.hpp"
 
@@ -11,8 +12,6 @@ using namespace std::chrono_literals;
 
 int main()
 {
-	auto async_in = rod::async_file::open("/dev/stdin", rod::async_file::in | rod::async_file::out);
-
 	rod::in_place_stop_source src;
 	rod::epoll_context ctx;
 	auto trd = std::jthread{[&]() { ctx.run(src.get_token()); }};
@@ -23,12 +22,18 @@ int main()
 		rod::sync_wait(rod::schedule_in(sch, 50ms) | rod::then([&]() { TEST_ASSERT((sch.now() - start) >= 50ms); }));
 	}
 	{
-		std::array<char, 13> buff = {};
-		//auto snd_read = rod::schedule(sch) | rod::async_read_some(async_in, std::span{buff});
-		//auto snd_write = rod::schedule_in(sch, 10ms) | rod::async_write_some(async_in, std::string_view{"hello, world\n"});
+		int pipe_fd[2] = {-1, -1};
+		::pipe2(pipe_fd, O_NONBLOCK | O_CLOEXEC);
 
-		auto snd0 = rod::transfer(rod::just(), sch);
-		rod::sync_wait(rod::async_write_some(snd0, async_in, std::span{buff}));
+		std::array<char, 13> buff = {};
+		const std::string_view data = "hello, world";
+		auto snd_read = rod::schedule(sch) | rod::async_read_some(pipe_fd[0], std::span{buff}) | rod::then([&](auto n) { TEST_ASSERT((std::string_view{buff.data(), n} == data)); });
+		auto snd_write = rod::schedule(sch) | rod::async_write_some(pipe_fd[1], data) | rod::then([&](auto n) { TEST_ASSERT(n == data.size()); });
+		rod::sync_wait(rod::when_all(snd_read, snd_write));
+
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
 	}
+
 	src.request_stop();
 }
