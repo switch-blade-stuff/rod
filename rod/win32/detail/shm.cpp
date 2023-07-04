3500238200a6508b3cfe5cedc0e963d584ea7853
switch-blade-stuff <terekhin.mikhail.01@gmail.com>
2023-07-04 06:01:06 -0700

    Updated win32 shared memory to use a named mutex instead of a spinlock

--- rod/win32/detail/shm.cpp
+++ rod/win32/detail/shm.cpp
@@ -6,21 +6,34 @@
 
 #include "shm.hpp"
 
+#ifdef __SSE2__
+#include <emmintrin.h>
+#endif
+
 #define NOMINMAX
+
 #include <windows.h>
-#include <intrin.h>
+#include <thread>
 
 namespace rod::detail
 {
+	/* Use the same name for both mutex & shared memory and differentiate via a postfix.
+	 * Postfix is used to allow for win32-esque "Global\" & "Local\" session namespaces. */
 	result<system_shm, std::error_code> system_shm::open(const char *name, std::size_t size, int mode) noexcept
 	{
 		try
 		{
-			if (const auto buff_size = ::MultiByteToWideChar(CP_UTF8, 0, name, -1, nullptr, 0); size) [[likely]]
+			if (const auto name_size = ::MultiByteToWideChar(CP_UTF8, 0, name, -1, nullptr, 0); size) [[likely]]
 			{
-				auto buff = std::wstring(buff_size - 1, '\0');
-				if (::MultiByteToWideChar(CP_UTF8, 0, name, -1, buff.data(), buff_size)) [[likely]]
-					return open(buff.c_str(), size, mode);
+				auto shm_name = std::wstring(name_size + 3, '\0');
+				auto mtx_name = std::wstring(name_size + 3, '\0');
+				if (::MultiByteToWideChar(CP_UTF8, 0, name, -1, shm_name.data(), name_size)) [[likely]]
+				{
+					mtx_name.replace(0, name_size - 1, shm_name);
+					shm_name.replace(name_size - 1, 3, L":shm");
+					mtx_name.replace(name_size - 1, 3, L":mtx");
+					return open(shm_name.c_str(), mtx_name.c_str(), size, mode);
+				}
 			}
 			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
 		}
@@ -31,19 +44,39 @@ namespace rod::detail
 	}
 	result<system_shm, std::error_code> system_shm::open(const wchar_t *name, std::size_t size, int mode) noexcept
 	{
-		native_handle_type hnd;
+		try
+		{
+			auto shm_name = std::wstring{name};
+			auto mtx_name = std::wstring{name};
+			shm_name.append(L":shm");
+			mtx_name.append(L":mtx");
+			return open(shm_name.c_str(), mtx_name.c_str(), size, mode);
+		}
+		catch (const std::bad_alloc &)
+		{
+			return std::make_error_code(std::errc::not_enough_memory);
+		}
+	}
+	result<system_shm, std::error_code> system_shm::open(const wchar_t *shm_name, const wchar_t *mtx_name, std::size_t size, int mode) noexcept
+	{
+		native_handle_type hnd, mtx = {};
 		if (mode & openmode::nocreate)
 		{
 			DWORD access = FILE_MAP_READ;
 			if (!(mode & openmode::readonly))
 				access |= FILE_MAP_WRITE;
+			hnd = ::OpenFileMappingW(access, false, shm_name);
 
-			hnd = ::OpenFileMappingW(access, false, name);
+			if (hnd != nullptr) [[likely]]
+			{
+				access = SYNCHRONIZE;
+				if (!(mode & openmode::readonly))
+					access |= MUTEX_MODIFY_STATE;
+				mtx = ::OpenMutexW(access, false, mtx_name);
+			}
 		}
 		else
 		{
-			size += sizeof(header_t);
-
 			DWORD prot = PAGE_READWRITE;
 			if (mode & openmode::readonly)
 				prot = PAGE_READONLY;
@@ -54,17 +87,16 @@ namespace rod::detail
 #endif
 			size_l = static_cast<DWORD>(size);
 
-			hnd = ::CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, prot, size_h, size_l, name);
+			if ((hnd = ::CreateFileMappingW(INVALID_HANDLE_VALUE, nullptr, prot, size_h, size_l, shm_name)) != nullptr)
+				[[likely]] mtx = ::CreateMutexW(nullptr, false, mtx_name);
 		}
 
-		if (hnd == nullptr) [[unlikely]]
+		if (hnd && !mtx)
+			::CloseHandle(hnd);
+		if (!hnd || !mtx) [[unlikely]]
 			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
-
-		const auto header = static_cast<header_t *>(::MapViewOfFile(hnd, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, sizeof(header_t)));
-		if (header == nullptr) [[unlikely]]
-			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
-
-		return system_shm{hnd, header};
+		else
+			return system_shm{hnd, mtx};
 	}
 
 	result<system_mmap, std::error_code> system_shm::map(std::size_t off, std::size_t n, int mode) const noexcept
@@ -78,7 +110,6 @@ namespace rod::detail
 			access |= FILE_MAP_WRITE;
 
 		/* Align the offset to page & header size. */
-		off += sizeof(header_t);
 		const auto base_off = system_mmap::pagesize_off(off);
 
 		DWORD off_h = {}, off_l;
@@ -86,26 +117,31 @@ namespace rod::detail
 		off_h = static_cast<DWORD>(base_off >> std::numeric_limits<DWORD>::digits);
 #endif
 		off_l = static_cast<DWORD>(base_off);
-ERROR_ALREADY_EXISTS;
+		ERROR_ALREADY_EXISTS;
 		if (const auto data = ::MapViewOfFile(native_handle(), access, off_h, off_l, n + off - base_off); !data) [[unlikely]]
 			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
 		else
 			return system_mmap{data, off - base_off, n + off - base_off};
 	}
 
-	void system_shm::lock() noexcept
+	bool system_shm::try_lock() noexcept
 	{
-		for (std::size_t spin = 16; !try_lock();)
-		{
-			if (!spin)
-				::YieldProcessor();
-			else
-				spin--;
-		}
+		const auto res = ::WaitForSingleObject(_mtx.native_handle(), 0);
+		return res == WAIT_OBJECT_0 || res == WAIT_ABANDONED;
 	}
-	bool system_shm::try_lock() noexcept { return !_header->busy.test_and_set(std::memory_order_acq_rel); }
-	void system_shm::unlock() noexcept { _header->busy.clear(std::memory_order_release); }
-
-	system_shm::~system_shm() { ::UnmapViewOfFile(_header); }
+	std::error_code system_shm::lock() noexcept
+	{
+		if (::WaitForSingleObject(_mtx.native_handle(), INFINITE) == WAIT_FAILED) [[unlikely]]
+			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
+		else
+			return {};
+	}
+	std::error_code system_shm::unlock() noexcept
+	{
+		if (!::ReleaseMutex(_mtx.native_handle())) [[unlikely]]
+			return std::error_code{static_cast<int>(::GetLastError()), std::system_category()};
+		else
+			return {};
+	}
 }
 #endif
